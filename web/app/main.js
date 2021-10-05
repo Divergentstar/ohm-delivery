30ca39b93ee87e099b53ca32c3e77d03a271c792
Ellen Peeters <peeters.ellen@yahoo.com>
2021-10-05 10:23:25 +0200

    reanme tobetter method names

--- web/app/main.js
+++ web/app/main.js
@@ -4,7 +4,7 @@ angular
 		const NOT_FOUND = (status, id) => (status === 404) ? `Sorry, the tracking ID #${id} was not found.` : ERROR();
 		const ERROR = () => 'Oops, this website is under construction, please come back later.';
 		$scope.formData = {};
-		$scope.get = function() {
+		$scope.getOhmByTrackingId = function() {
 			if (this.trackingId) {
 				$http.get(`/ohms/${this.trackingId}`)
 					.then((result) => {
@@ -19,18 +19,15 @@ angular
 				$scope.errorMessage = 'Please choose a tracking ID.';
 			}
 		};
-		$scope.updateStatus = function(status) {
-			console.log('iets');
+		$scope.updateOhmStatus = function(status) {
 			if (this.trackingId) {
 				$http.put(`/ohms/${this.trackingId}/status`, {
 					status: status,
 					comment: this.formData.comment[status]
 				}).then((result) => {
-					console.log('res');
 					$scope.result = result.data;
 					$scope.errorMessage = '';
 				}, (error) => {
-					console.log('err');
 					$scope.result = null;
 					$scope.errorMessage = NOT_FOUND(error.status, $scope.trackingId);
 				});
